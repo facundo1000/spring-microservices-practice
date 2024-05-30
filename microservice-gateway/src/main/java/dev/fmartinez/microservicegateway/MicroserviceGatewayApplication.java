665af0a0a3b9cbc611cf3cea64f44c9fc8e538c1
facundo1000 <martinez.facundo85@gmail.com>
2024-05-29 21:52:18 -0300

    add gateway module

---
+++ microservice-gateway/src/main/java/dev/fmartinez/microservicegateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.fmartinez.microservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayApplication.class, args);
+    }
+
+}
